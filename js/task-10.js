9df4dc6f431672e2b0ec1f799f32d9f668a06e06
Lana2408 <119637611+Lana2408@users.noreply.github.com>
2023-07-16 20:08:59 +0100

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,3 +3,36 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+
+const createBtn = document.querySelector("[data-create]");
+const destroyBtn = document.querySelector("[data-destroy]");
+const inputEl = document.querySelector(`input`);
+const boxesDiv = document.querySelector("#boxes");
+
+createBtn.addEventListener("click", () => {
+  const amount = Number(inputEl.value);
+  createBoxes(amount);
+});
+
+destroyBtn.addEventListener("click", destroyBoxes);
+
+function createBoxes(amount) {
+  const initialSize = 30;
+  const fragment = document.createDocumentFragment();
+
+  for (let i = 0; i < amount; i++) {
+    const div = document.createElement("div");
+    const size = initialSize + i * 10;
+    div.style.width = `${size}px`;
+    div.style.height = `${size}px`;
+    div.style.backgroundColor = getRandomHexColor();
+    fragment.appendChild(div);
+  }
+
+  boxesDiv.appendChild(fragment);
+}
+
+function destroyBoxes() {
+  boxesDiv.innerHTML = "";
+}
